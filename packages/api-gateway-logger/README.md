5314eda2438a507c7cbfaee6d0619bd35e670d7f
Rodolfo Canessa <rcanessa89@hotmail.com>
2025-01-23 01:59:19 -0600

    feat(new package): adding new package api gateway logger

---
+++ packages/api-gateway-logger/README.md
@@ -0,0 +1,11 @@
+# api-gateway-logger
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build api-gateway-logger` to build the library.
+
+## Running unit tests
+
+Run `nx test api-gateway-logger` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/api-gateway-logger/src/index.ts
@@ -0,0 +1,41 @@
+import { type APIGatewayProxyEventV2, type Context } from 'aws-lambda';
+import { getLogger } from '@rcanessa/node-logger';
+
+export const apiGatewayLogger = (
+  name: string,
+  event: APIGatewayProxyEventV2,
+  context: Context
+): ReturnType<typeof getLogger> => {
+  const {
+    version,
+    headers,
+    queryStringParameters,
+    pathParameters,
+    requestContext
+  } = event;
+
+  return getLogger({
+    name,
+    level: process.env['AWS_LAMBDA_LOG_LEVEL'] || 'info',
+    formatters: {
+      bindings: () => ({
+        nodeVersion: process.version,
+        requestId: context.awsRequestId,
+        function: process.env['AWS_LAMBDA_FUNCTION_NAME']
+      }),
+      log: (obj: unknown) => ({
+        event: {
+          version,
+          headers,
+          queryStringParameters,
+          pathParameters,
+          requestContext
+        },
+        metadata: {
+          ...(obj || {})
+        }
+      })
+    },
+    redact: ['event.headers.Authorization', 'event.headers.authorization']
+  });
+};
